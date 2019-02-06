8693fdfcdfffcb9881ff3cbf2548c21eecececc3
Lisa Burton <hello@paper.earth>
2019-02-06 02:07:35 -0500

    cleanup

---
+++ js/temp.js
@@ -0,0 +1,28 @@
+
+
+            // CUSTOM set height to zero
+            currentPanel.style.height = '0px';
+
+
+
+
+
+
+	// Get the natural height of the element
+	var getHeight = function () {
+		elem.style.display = 'block'; // Make it visible
+		var height = elem.scrollHeight + 'px'; // Get it's height
+		elem.style.display = ''; //  Hide it again
+		return height;
+	};
+
+	var height = getHeight(); // Get the natural height
+	elem.classList.add('is-visible'); // Make the element visible
+	elem.style.height = height; // Update the max-height
+
+	// Once the transition is complete, remove the inline max-height so the content can scale responsively
+	window.setTimeout(function () {
+		elem.style.height = '';
+	}, 350);
+
+
