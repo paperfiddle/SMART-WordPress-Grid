46f7c8321d7718489da4b1ae45b4c37f543d9f3a
wcDogg <ljburton@gmail.com>
2019-03-28 14:34:18 -0400

    Refactored Vanilla Idify Linkify Tocify

--- js/linkify.js
+++ js/linkify.js
@@ -2,182 +2,163 @@
  * Vanilla Idify Linkify Tocify
  *
  * @this /js/linkify.js
- * @css /sass/components/_linkify.scss 
- * 
- * Idify - Adds missing 'id' attribute to all headings 
+ * @css /sass/components/_linkify.scss
+ *
+ * Idify - Adds missing 'id' attribute to all headings
  * Linkify - Adds an anchor tag with icon to defined heading levels
  * Tocify - Creates a table of contents from defined heading levels
- * 
- * NO SPECIAL markup needed in .html, BUT search this file for 'EDIT THIS'
- * 
- * TOC container is output as: section -> heading, nav -> div -> a
- * This structure supports future plan to add toggle.
- * It's also semantically correct + proper ARIA attributes are present.
- * 
- * 
- * Inspired by Parker Moore's work for jekyllrb.com: 
+ *
+ * Inspired by Parker Moore's work for jekyllrb.com:
  * https://byparker.com/blog/2014/header-anchor-links-in-vanilla-javascript-for-github-pages-and-jekyll/
- * 
- * 
+ *
  */
 
+function LinkTocify(selector, linkLevel, tocLevel) {
 
-(function () {
-
-    // EDIT THIS: Look in these containers for headings
-    // If using anything other than 'getElementById', 
-    // then [0] index only the first instance so we don't
-    // have to iterate the containers. 
-    var headings_container = document.getElementsByClassName('post__main')[0] ||
-        document.getElementsByClassName('page__main')[0];
+    // The container with headings to linkify, tocify
+    let container = document.querySelector(selector);
 
 
-    // Get all headings in the containers and iterate
-    // Add missing 'id' attributes to all headings
-    // Get all information needed by linkify() and tocify()
+    // Get all headings, assign missing 'id's, pass to linkify +  tocify
     function idify() {
 
-        var headings_all = headings_container.querySelectorAll('h1, h2, h3, h4, h5, h6');
+        let headings = Array.from(
+            container.querySelectorAll('h1, h2, h3, h4, h5, h6')
+        );
 
-        for (var i = 0; i < headings_all.length; i++) {
+        headings.forEach(function (heading) {
 
-            // This heading
-            var heading = headings_all[i];
+            // This heading's tag returned as 'h1' 'h2' 'h3'
+            let headingTag = heading.tagName.toLowerCase();
 
-            // This heading's tag returned as 'h1' 'h2' 'h3' ...
-            var heading_tag = heading.tagName.toLowerCase();
+            // Remove 'h' so we can use number to compare
+            let headingLevel = headingTag.substring(1);
 
             // This heading's unaltered text as a string
-            var heading_text = heading.textContent;
+            let headingText = heading.textContent;
 
-            // String with special characters and 
-            // leading-trailing white space trimmed
-            var heading_prep = heading_text.replace(/[^\w\s]/gi, '').trim();
-            
-            // String with space to dash + lowercase
-            var heading_id = heading_prep.replace(/\s+/g, '-').toLowerCase();
+            // The heading's id 
+            let headingId = headingText
+                .replace(/[^\w\s]/gi, ' ') // remove special characters
+                .toLowerCase() // make lowercase            
+                .trim() // trim leading + trailing white space
+                .replace(/\s+/g, '-') // remplace space with dash
+                .substring(0, 24); // return first 24 characters
 
-            // Add missing ids
+            // Add missing 'id's to all headings
             if (typeof heading.id == 'undefined' || heading.id == null || heading.id == '') {
-                heading.setAttribute('id', heading_id);
+                heading.setAttribute('id', headingId);
+            }; 
+
+            // Pass to linkify
+            if (headingLevel <= linkLevel) {
+                linkify(heading, headingTag);
+            }; 
+
+            // Pass to tocify
+            if (tocLevel > 0 && headingLevel <= tocLevel) {
+                tocify(heading, headingText, headingTag);
             }
 
-            // Pass headings to linkify and tocify
-            linkify(heading, heading_tag, heading_text, heading.id );
-            tocify(heading, heading_tag, heading_text, heading.id);
-
-            // console.log('idified heading:', heading);            
-
-        }// for
-       
+        }); // forEach
     }; // idify
 
 
-    // Linkify headings
-    function linkify(heading, tag, text, id) {
+    // Add anchors to headings
+    function linkify(heading, headingText) {
 
-        // EDIT THIS: If you want to limit which headings are linkified,
-        // then move all code into a conditional statement like this:
+        heading.setAttribute('data-linkify-heading', '');
 
-        // if (tag == 'h1' || tag == 'h2') {
-        //     // code here
-        // }; 
-
-        function makeAnchor() {
-            var anchor = document.createElement('a');
-            anchor.className = 'linkify';
+        let anchor = document.createElement('a');
+            anchor.setAttribute('data-linkify-anchor', '');        
             anchor.setAttribute('rel', 'bookmark');
-            anchor.setAttribute('title', text);
-            anchor.href = '#' + id;
-            // anchor.innerHTML = '<span class="icon-link"></span>';
-            anchor.innerHTML = '<svg aria-hidden="true" focusable="false" class="linkify__icon" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="currentColor" d="M12.971 352h32.394C67.172 454.735 181.944 512 288 512c106.229 0 220.853-57.38 242.635-160h32.394c10.691 0 16.045-12.926 8.485-20.485l-67.029-67.029c-4.686-4.686-12.284-4.686-16.971 0l-67.029 67.029c-7.56 7.56-2.206 20.485 8.485 20.485h35.146c-20.29 54.317-84.963 86.588-144.117 94.015V256h52c6.627 0 12-5.373 12-12v-40c0-6.627-5.373-12-12-12h-52v-5.47c37.281-13.178 63.995-48.725 64-90.518C384.005 43.772 341.605.738 289.37.01 235.723-.739 192 42.525 192 96c0 41.798 26.716 77.35 64 90.53V192h-52c-6.627 0-12 5.373-12 12v40c0 6.627 5.373 12 12 12h52v190.015c-58.936-7.399-123.82-39.679-144.117-94.015h35.146c10.691 0 16.045-12.926 8.485-20.485l-67.029-67.029c-4.686-4.686-12.284-4.686-16.971 0L4.485 331.515C-3.074 339.074 2.28 352 12.971 352zM288 64c17.645 0 32 14.355 32 32s-14.355 32-32 32-32-14.355-32-32 14.355-32 32-32z"></path></svg>';
+            anchor.setAttribute('title', headingText);
+            anchor.href = '#' + heading.id;
 
-            return anchor;             
-        };            
+        anchor.innerHTML = '<svg aria-hidden="true" focusable="false" data-linkify-icon class="icon__svg" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M440.667 182.109l7.143-40c1.313-7.355-4.342-14.109-11.813-14.109h-74.81l14.623-81.891C377.123 38.754 371.468 32 363.997 32h-40.632a12 12 0 0 0-11.813 9.891L296.175 128H197.54l14.623-81.891C213.477 38.754 207.822 32 200.35 32h-40.632a12 12 0 0 0-11.813 9.891L132.528 128H53.432a12 12 0 0 0-11.813 9.891l-7.143 40C33.163 185.246 38.818 192 46.289 192h74.81L98.242 320H19.146a12 12 0 0 0-11.813 9.891l-7.143 40C-1.123 377.246 4.532 384 12.003 384h74.81L72.19 465.891C70.877 473.246 76.532 480 84.003 480h40.632a12 12 0 0 0 11.813-9.891L151.826 384h98.634l-14.623 81.891C234.523 473.246 240.178 480 247.65 480h40.632a12 12 0 0 0 11.813-9.891L315.472 384h79.096a12 12 0 0 0 11.813-9.891l7.143-40c1.313-7.355-4.342-14.109-11.813-14.109h-74.81l22.857-128h79.096a12 12 0 0 0 11.813-9.891zM261.889 320h-98.634l22.857-128h98.634l-22.857 128z"></path></svg>';
 
-        heading.appendChild(makeAnchor());
-
-        // console.log('linkify heading:', heading);   
-            
-    }; // linkify
+        // Anchor before heading
+        // This is the best postion in terms of markup.
+        // Use CSS to visually display icon before or after heading.
+        heading.insertBefore(anchor, heading.firstChild);
+        
+    }; //
 
 
-    // Make the tocify TOC container
-    var tocifyToc = function () {
+    // Prepare a TOC section to receive items
+    function tocifyToc() {
 
-        // Create DOM elements
-        var toc_container = document.createElement('section');
-        toc_container.id = 'tocify'
-        toc_container.className = 'tocify';
+        let tocContainer = document.createElement('section');
+        tocContainer.setAttribute('data-tocify-toc', '');
 
-        var toc_heading = document.createElement('h1');
-        toc_heading.id = 'tockify-heading';
-        toc_heading.className = 'tocify__heading';
-        toc_heading.innerHTML = 'On This Page';
+        let tocTitle = document.createElement('h1');
+        tocTitle.setAttribute('data-tocify-title', '');
+        tocTitle.innerHTML = 'On This Page';
 
-        var toc_nav = document.createElement('nav');
-        toc_nav.id = 'tocify-nav';
-        toc_nav.className = 'tocify__nav';
-        toc_nav.setAttribute('role', 'menu');
-        toc_nav.setAttribute('aria-label', 'Page contents');
+        let tocNav = document.createElement('nav');
+        tocNav.setAttribute('data-tocify-nav', '');
+        tocNav.setAttribute('role', 'menu');
+        tocNav.setAttribute('aria-label', 'Page contents');
 
         // Append DOM
-        var toc_target = headings_container;
-        toc_target.parentNode.insertBefore(toc_container, toc_target);
-        toc_container.appendChild(toc_heading);
-        toc_container.appendChild(toc_nav);
+        container.parentNode.insertBefore(tocContainer, container);
+        tocContainer.appendChild(tocTitle);
+        tocContainer.appendChild(tocNav);
 
-    };// tocifyToc
-
-
-    // Tocify headings
-    function tocify(heading, tag, text, id) {
-
-        // EDIT THIS: If you want to limit which headings are tocified,
-        // then move all code into a conditional statement like this:
-
-        // if (tag == 'h1' || tag == 'h2') {
-        //     // code here
-        // }; 
-
-        // Make link 
-        var anchor = document.createElement('a');
-            anchor.className = 'tocify__link tocify__link--' + tag;
-            anchor.setAttribute('rel', 'bookmark');
-            anchor.setAttribute('title', text);
-            anchor.setAttribute('role', 'menuitem');
-            anchor.href = '#' + id;
-            anchor.innerHTML = text;
-
-        // Make link item wrapper
-        var nav_item = document.createElement('div');
-            nav_item.className = 'tocify__nav-item';
-            nav_item.setAttribute('role', 'none');
-            nav_item.appendChild(anchor);
-
-        // Add links to TOC
-        document.getElementById('tocify-nav').append(nav_item);   
-
-        // console.log('tocify heading:', heading);
-
-    }; // tocify
+    }; //
 
 
-    // Listen for DOM 
-    document.addEventListener('DOMContentLoaded', function (event) {
+    function tocify(heading, headingText, headingTag) {
 
-        // If there is no container we want, then return
-        if (!headings_container) {
-            console.log('LOADED linkify.js but no heading containers were found');
-            return; 
-        // Else go ahead and and do some stuff
-        } else {
-            tocifyToc();
-            idify();
-            console.log('LOADED linkify.js');
-        }
+        heading.setAttribute('data-tocify-heading', '');
 
-    }); // listen  
+        let tocLink = document.createElement('a');
+            tocLink.setAttribute('data-tocify-link', '');
+            tocLink.setAttribute('data-tocify-' + headingTag, '');
+            tocLink.setAttribute('rel', 'bookmark');
+            tocLink.setAttribute('title', headingText);
+            tocLink.href = '#' + heading.id;       
+            tocLink.innerHTML = headingText;
 
-})();    
+        let tocItem = document.createElement('div'); 
+            tocItem.setAttribute('role', 'none');
+            tocItem.setAttribute('data-tocify-item', ''); 
+            tocItem.appendChild(tocLink);
 
+        let tocTarget = document.querySelector('[data-tocify-nav]')
+        tocTarget.append(tocItem);
+
+        console.log('Tocify:', tocItem);
+        
+    }; //
+
+
+    // Run the functions
+    if (tocLevel > 0) {
+        tocifyToc();
+    }
+
+    idify();
+
+};  // LinkTocify
+
+
+//
+// HTML
+// @param selector - '.class' or '#id'
+// @param integer - Heading level to linkify to
+// @param optional integer - Heading level to tocify to
+// 
+// <script>
+//      LinkTocify.init('.post__main', 6, 6); 
+// <script>
+//
+
+ LinkTocify.init = function (selector, linkLevel, tocLevel) {
+
+     console.log('INIT linkify.js');
+     return new LinkTocify(selector, linkLevel, tocLevel);
+
+ }
+
+ 
